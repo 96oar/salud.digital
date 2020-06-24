8b3f24559f3bafd076d98b6b17287ec0b3f671dc
Adrian Rao <oar.adrian@gmail.com>
2020-06-23 23:42:21 -0300

    Arreglo de conflictos y se agrega el select de hospitales.

---
+++ js/hospitales.js
@@ -0,0 +1,39 @@
+$(document).ready(() => {
+    $(".hospitals").select2({
+        width: "resolve",
+        placeholder: "Seleccione un Hospital..."
+    });
+
+    $(".hospitals").change(() => {
+        let hospital = $(".hospitals").val();
+
+        let location = "";
+        let direction = "";
+
+        switch (hospital) {
+            case "buenPastor":
+                direction = "Av. Gral. Enrique Mosconi 1716";
+                location = "Lomas del Mirador, Provincia de Buenos Aires";
+                break;
+            case "moron":
+                direction = "Dr. Rodolfo Monte 848";
+                location = "Moron, Provincia de Buenos Aires";
+                break;
+            case "belgrano":
+                direction = "Av. de los Constituyentes 3120";
+                location = "San Andres, Provincia de Buenos Aires";
+                break;
+
+            default:
+                break;
+        }
+        if (direction != "" && location != "") {
+            alertify
+                .alert(
+                    `<p><strong>Direccion:</strong> ${direction}</p>
+                <p><strong>Localidad:</strong> ${location}</p>`
+                )
+                .set("resizable", false);
+        }
+    });
+});
