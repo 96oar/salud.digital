22fb2f2288bb108a88ce698e73c82ff9a9af6fed
Adrian Rao <oar.adrian@gmail.com>
2020-06-24 00:30:53 -0300

    Se hacen correcciones de codigo, identado y descargar componentes locales.

--- js/hospitales.js
+++ js/hospitales.js
@@ -1,39 +1,43 @@
 $(document).ready(() => {
-    $(".hospitals").select2({
-        width: "resolve",
-        placeholder: "Seleccione un Hospital..."
-    });
+  $(".hospitals").select2({
+    width: "resolve",
+    placeholder: "Seleccione un Hospital...",
+  });
 
-    $(".hospitals").change(() => {
-        let hospital = $(".hospitals").val();
+  $(".hospitals").change(() => {
+    let hospital = $(".hospitals").val();
 
-        let location = "";
-        let direction = "";
+    let location = "";
+    let direction = "";
 
-        switch (hospital) {
-            case "buenPastor":
-                direction = "Av. Gral. Enrique Mosconi 1716";
-                location = "Lomas del Mirador, Provincia de Buenos Aires";
-                break;
-            case "moron":
-                direction = "Dr. Rodolfo Monte 848";
-                location = "Moron, Provincia de Buenos Aires";
-                break;
-            case "belgrano":
-                direction = "Av. de los Constituyentes 3120";
-                location = "San Andres, Provincia de Buenos Aires";
-                break;
+    switch (hospital) {
+      case "buenPastor":
+        direction = "Av. Gral. Enrique Mosconi 1716";
+        location = "Lomas del Mirador, Provincia de Buenos Aires";
+        break;
+      case "moron":
+        direction = "Dr. Rodolfo Monte 848";
+        location = "Moron, Provincia de Buenos Aires";
+        break;
+      case "belgrano":
+        direction = "Av. de los Constituyentes 3120";
+        location = "San Andres, Provincia de Buenos Aires";
+        break;
 
-            default:
-                break;
-        }
-        if (direction != "" && location != "") {
-            alertify
-                .alert(
-                    `<p><strong>Direccion:</strong> ${direction}</p>
-                <p><strong>Localidad:</strong> ${location}</p>`
-                )
-                .set("resizable", false);
-        }
-    });
+      default:
+        break;
+    }
+    if (direction != "" && location != "") {
+      alertify
+        .alert(
+            `<p><strong>Direccion:</strong> ${direction}</p>
+            <p><strong>Localidad:</strong> ${location}</p>`
+        )
+        .set({
+          title: "Informacion sobre el hospital",
+          transition: "fade",
+          movable: false,
+        });
+    }
+  });
 });
